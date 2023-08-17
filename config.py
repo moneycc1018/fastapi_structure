21a70d1611583d72017f34ac3b12505c1d0bbf5d
dukechien <ccrr0101@gmail.com>
2023-08-17 18:55:05 +0800

    update images

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 APP_TITLE="FastAPI Structure"
-APP_DESCRIPION="FastAPI公版架構"
+APP_DESCRIPION="FastAPI 架構"
 ALLOW_ORIGINS=os.getenv("ALLOW_ORIGINS", "http://0.0.0.0:3333")
 TEST=os.getenv("TEST", "NO MONEY")
