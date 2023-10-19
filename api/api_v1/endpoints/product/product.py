ebb5d2bd695232bb8db76236380acd37f2b0af44
Jerry Hung <poplol0900@gmail.com>
2023-10-19 16:57:11 +0800

    feat: Add basic API authentication with API key in header.

--- api/api_v1/endpoints/product/product.py
+++ api/api_v1/endpoints/product/product.py
@@ -1,15 +1,16 @@
 from fastapi import APIRouter, HTTPException
 
 from schemas.product import productDetail
+from .auth import get_api_key
 
 router = APIRouter()
 
 @router.get("/{product_id}")
-async def readProductId(product_id: str):
+async def readProductId(product_id: str, api_key: str = Depends(get_api_key)):
     return {"product_id": product_id}
 
 @router.put("/detail/{product_id}")
-async def readProductDetail(product_id: str, model: productDetail):
+async def readProductDetail(product_id: str, model: productDetail, api_key: str = Depends(get_api_key)):
     model_dict = model.dict()
     if model.price == None:
         raise HTTPException(status_code=404, detail="No price!")
